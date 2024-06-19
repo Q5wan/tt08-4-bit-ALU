2bb148452e684ccf01932a762120b5b45fe126b5
Ben Landon <149592168+Q5wan@users.noreply.github.com>
2024-06-19 10:53:27 +0200

    Update 4-bit-ALU.v

--- src/4-bit-ALU.v
+++ src/4-bit-ALU.v
@@ -5,32 +5,7 @@
 
 `default_nettype none
 
-module tt_um_Q5wan_4_bit_ALU (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  // Instantiate the 4-bit ALU module
-  ALU ALU_instance (
-      .ui_in(ui_in),
-      .uo_out(uo_out),
-      .uio_in(uio_in),
-      .uio_out(uio_out),
-      .uio_oe(uio_oe),
-      .ena(ena),
-      .clk(clk),
-      .rst_n(rst_n)
-  );
-
-endmodule
-
-module ALU (
+module tt_um_Q5wan_4_bit_ALU  (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
